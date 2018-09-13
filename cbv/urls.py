ae35c3ff0e0d8583c3097f1cf46437a41e6d5147
Fayomi Fash <fayomifash@Fayomis-MacBook-Pro-2.local>
2018-09-13 18:21:08 +0100

    CRUD Commit

--- cbv/urls.py
+++ cbv/urls.py
@@ -6,7 +6,10 @@ app_name = 'cbv'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('schools',views.SchoolListView.as_view(),name='list'),
-    path('schools/<pk>/', views.SchoolDetailView.as_view(), name='detail')
+    path('schools/<pk>/', views.SchoolDetailView.as_view(), name='detail'),
+    path('create',views.SchoolCreateView.as_view(), name='create'),
+    path('update/schools/<pk>/', views.SchoolUpdateView.as_view(), name='update'),
+    path('delete/schools/<pk>/', views.SchoolDeleteView.as_view(), name='delete'),
 
 
 ]
--- cbv/views.py
+++ cbv/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView,ListView,DetailView
+from django.views.generic import (View,
+TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from . import models
+from django.urls import reverse_lazy
+
 
 
 class IndexView(TemplateView):
@@ -14,3 +17,16 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'cbv/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields = ('name','principal','location')
+
+class SchoolUpdateView(UpdateView):
+    model = models.School
+    fields = ('name','principal')
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('cbv:list')
